6a0cc66210b4b5e86bcdebda2e98b2b304b55662
Matt Oestreich <matthewpoestreich@gmail.com>
2020-08-19 15:22:06 -0500

    update

--- main.go
+++ main.go
@@ -15,17 +15,15 @@ func main() {
 // GetIPHandler handles the get ip address request
 func GetIPHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
-	resp, _ := json.Marshal(map[string]string{
-		"ip": GetIP(r),
-	})
+	resp, _ := GetIP(r)
 	w.Write(resp)
 }
 
 // GetIP gets a requests IP address by reading off the x-real-ip
-func GetIP(r *http.Request) string {
-	real := r.Header.Get("X-REAL-IP")
-	if real != "" {
-		return real
-	}
-	return r.RemoteAddr
+func GetIP(r *http.Request) ([]byte, error) {
+	return json.Marshal(map[string]string{
+		"realIp":        r.Header.Get("x-forwarded-for"),
+		"forwardedFor":  r.Header.Get("x-real-ip"),
+		"remoteAddress": r.RemoteAddr,
+	})
 }
